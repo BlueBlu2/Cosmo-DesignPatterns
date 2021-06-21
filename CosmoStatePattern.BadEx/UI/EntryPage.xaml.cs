8374f4797fb3e45e4e735737544e571c62902cf4
Blue <eng.abdalrhmn.mohamed@gmail.com>
2021-06-21 17:53:22 +0200

    Add project files.

---
+++ CosmoStatePattern.BadEx/UI/EntryPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CosmoStatePattern.BadEx.UI
+{
+    /// <summary>
+    /// Interaction logic for EntryPage.xaml
+    /// </summary>
+    public partial class EntryPage : Page
+    {
+        public EntryPage()
+        {
+            InitializeComponent();
+        }
+        private void ValidateNumber(Object sender, TextCompositionEventArgs e)
+        {
+            Regex regX = new Regex("[^0-9]");
+            e.Handled = regX.IsMatch(e.Text);
+        }
+    }
+}
